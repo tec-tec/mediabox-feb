8c4c9f5ef49d71d6042876a3dbb61d78673ddbc1
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-02-23 17:48:22 +0100

    Basic restaurantList

--- RestOrank/RestaurantListTableViewController.h
+++ RestOrank/RestaurantListTableViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface RestaurantListTableViewController : UITableViewController
+@interface RestaurantListTableViewController : UITableViewController <UITableViewDataSource, UITableViewDelegate>
 
 @end
--- RestOrank/RestaurantListTableViewController.m
+++ RestOrank/RestaurantListTableViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "RestaurantListTableViewController.h"
+#import "RestaurantManager.h"
+#import "Restaurant.h"
 
 @interface RestaurantListTableViewController ()
+
+@property (strong, nonatomic) RestaurantManager *manager;
 
 @end
 
@@ -16,12 +20,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -31,68 +29,32 @@
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
-    return 0;
+
+    return [[self.manager allRestaurants] count];
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RestoCell" forIndexPath:indexPath];
     
     // Configure the cell...
-    
+    Restaurant *currentResto = self.manager.allRestaurants[indexPath.row];
+    cell.textLabel.text = currentResto.name;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", currentResto.grade];
+
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (RestaurantManager *)manager {
+
+    if (_manager == nil) {
+        _manager = [[RestaurantManager alloc] init];
+        [_manager addRestaurant:[[Restaurant alloc] initWithName:@"McDo"]];
+    }
+
+    return _manager;
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
