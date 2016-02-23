0cdd6cbf74c81c2983f58dcf4e4ea4d9c866a164
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-02-23 14:19:16 +0100

    added an empty restaurantManager class

---
+++ RestOrank/RestaurantManager.h
@@ -0,0 +1,13 @@
+//
+//  RestaurantManager.h
+//  RestOrank
+//
+//  Created by Ludovic Ollagnier on 23/02/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RestaurantManager : NSObject
+
+@end
---
+++ RestOrank/RestaurantManager.m
@@ -0,0 +1,13 @@
+//
+//  RestaurantManager.m
+//  RestOrank
+//
+//  Created by Ludovic Ollagnier on 23/02/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+#import "RestaurantManager.h"
+
+@implementation RestaurantManager
+
+@end
